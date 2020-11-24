c9663de83de61d7d6f66494c8160fd7e080e6ef8
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-11-24 14:35:21 +0300

    add functions

--- include/Automata.h
+++ include/Automata.h
@@ -1,46 +1,50 @@
 #pragma once
 #ifndef AUTOMATA_H
 #define AUTOMATA_H
-#include <iostream>
+#include <fstream>
+#include <string>
 #include <map>
 
 
 using namespace std;
-const map <string, int> menu = { {"Milk"   , 10},
-								{"Water"  ,  5},
-								{ "Tea"   , 30},
-								{"Coffee" , 40},
-								{"Cola"   , 50} };
-//const vector <string> menu = { "Milk","Water","Tea", "Coffee","Cola" };
+
 enum States {
 	OFF,
 	WAIT,
 	ACCEPT,
 	CHECK,
-	COOK,
-	//ERROR, 
-	//FINISH
+	COOK
 };
 
-
-
 class Automata {
+
 private:
-	
 	int money;
 	States state;
 	map<string,int> menu;
+	map <string,int> users_choice;
+	int profit;
+	ostream* input_stream;
+	
 public:
-    Automata();
+    Automata(ostream*);
+	Automata();
+    void printMenu();
+	void setSettings();
+	
 	void setMenu();
 	void on();
 	void off();
-	void coin();
-	void getMenu();
-	void getState();
-	void choice();
+	void coin(int cash);
+	map <string,int> getMenu();
+	States getState();
+	void choice(string product);
 	void cancel();
 	void cook();
 	void finish();
+	void reset();
+	void return_money();
+	int getProfit();
 };
 #endif 
+
